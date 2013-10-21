ceeb11ddab0a99b47f6df79d7434da775f3042b0
key60529 <lunhoyin@lun-ho-yinde-macbook-pro.local>
2013-10-21 18:41:49 +0800

    Update Website

--- js/MainMenu.js
+++ js/MainMenu.js
@@ -1,24 +1,18 @@
 // JavaScript Document
 $(function() {
-	var state = true;
 	$('.menuButton').click(function() {
-		if (state) {
 			$('.menuContainer').animate({
 				width: "80%",
 				opacity: 1}, 500);
-				}	state = false;
-	});
+});
 });
 
-/*
+
 $(function(){
 	$('.menuCloseBn').click(function() {
-		if (state == false) {
 			$('.menuContainer').animate({
 				width: "0",
 				opcaity: 0}, 500);
-		}	state = !state;
 	});
 });
 
-*/
