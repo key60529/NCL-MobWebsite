7fd4efd82f7d94f0d71f5744f0fd05e78dad19b3
key60529 <lunhoyin@lun-ho-yinde-macbook-pro.local>
2013-10-28 10:40:22 +0800

    final update
    final update

---
+++ js/checkmob.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) {
+	 if (document.cookie.indexOf("iphone_redirect=false") == -1){ window.location = "http://www.newcreativeadv.com/testmobweb/m_index.html";}
+}
+
+if(navigator.userAgent.match(/Android/i)) {
+  // Redirect to Android-site?
+  window.location = 'http://www.newcreativeadv.com/testmobweb/m_index.html';
+}
